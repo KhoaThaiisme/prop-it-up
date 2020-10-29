10eb62fa19965a4f516fac8b0b8cd2b18442dba6
KhoaThaiisme <66451988+KhoaThaiisme@users.noreply.github.com>
2020-10-29 17:34:42 -0500

    commit2

--- src/components/myComponents.js
+++ src/components/myComponents.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
 
+
+
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age,
+            commt: ""
+        }
+    }
+    addAge = () => {
+        this.state.age += 1;
+        this.setState({ age: this.state.age })
+        if (this.state.age >= 60) {
+            this.setState({ commt: "Wow, you've grown up that well!!!" })
+        }
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.props.firstName} {this.props.lastName}</h1>
-                <p>age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair color: {this.props.hairColor}</p>
+                <button onClick={this.addAge}>Add</button>
+                <p>{this.state.commt}</p>
             </div>
         )
     }
